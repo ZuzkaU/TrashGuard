2d944fb7ac132790dcc3385baafee8ecd9274aab
Xinyi Cui <ge65tey@mytum.de>
2021-11-20 18:25:39 +0100

    added signup

--- login_signup/login.py
+++ login_signup/login.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/profile')
-def profile():
+@app.route('/login')
+def login():
        return render_template('login.html') 
+
+@app.route('/signup')
+def signup():
+       return render_template('signup.html')        
+
 
 if __name__ == '__main__':
    app.run('127.0.0.1', port=8000, debug = True)
