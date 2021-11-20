97b9faec5ec62d93d60cb69843c1905bdf6ede13
Xinyi Cui <ge65tey@mytum.de>
2021-11-20 17:58:17 +0100

    added login signup

---
+++ login_signup/login.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/profile')
+def profile():
+       return render_template('login.html') 
+
+if __name__ == '__main__':
+   app.run('127.0.0.1', port=8000, debug = True)
