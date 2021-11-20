8a834c61f3bd11edc5c2aff2f0eb90fc5e798ac5
Xinyi Cui <ge65tey@mytum.de>
2021-11-20 19:25:17 +0100

    working

--- login_signup/login.py
+++ login_signup/login.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-       return render_template('login.html') 
+	if request.method == 'GET':
+		return render_template('login.html') 
 
-@app.route('/signup')
+	if request.method == 'POST':
+		uname = request.form['uname']
+		pword = request.form['pword']
+		return uname
+
+@app.route('/signup', methods = ['GET', 'POST'])
 def signup():
-       return render_template('signup.html')        
-
+	if request.method == 'GET':
+		return render_template('signup.html')        
+	
+	if request.method == 'POST':
+		fname = request.form['fname']
+		lname = request.form['lname']
+		email = request.form['email']
+		uname = request.form['uname']
+		pword = request.form['pword']
+		return email
 
 if __name__ == '__main__':
-   app.run('127.0.0.1', port=8000, debug = True)
+	app.run('127.0.0.1', port=8000, debug = True)
