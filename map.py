07d962ce7bca6a8af80ed6ddfe604638f6df0799
Zuzana Urbanová <zu10urbanova@gmail.com>
2021-11-21 01:03:49 +0100

    separate layers

--- map.py
+++ map.py
@@ -14,34 +14,33 @@ def getMap():
     lat, lon = 48.11004353217281, 11.587360738996582
 
     id_counter = 0
-    markers = ''
     
     api = overpy.Overpass()
 
-
+    baskets = ''
     waste_baskets_query = """(node["amenity"="waste_basket"](around:{area},{lat},{lon}););out;""".format(area=area, lat=lat, lon=lon)
     waste_baskets = api.query(waste_baskets_query)
     for node in waste_baskets.nodes:
-        markers += "markers.addLayer(L.marker([{latitude}, {longitude}], {{icon: waste_basket_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Waste basket');
+        baskets += "baskets.addLayer(L.marker([{latitude}, {longitude}], {{icon: waste_basket_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Waste basket');
 
-
+    disposal = ''
     waste_disposal_query = """(node["amenity"="waste_disposal"](around:{area},{lat},{lon}););out;""".format(area=area, lat=lat, lon=lon)
     waste_disposal = api.query(waste_disposal_query)
     for node in waste_disposal.nodes:
-        markers += "markers.addLayer(L.marker([{latitude}, {longitude}], {{icon: waste_disposal_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Waste disposal');
+        disposal += "disposal.addLayer(L.marker([{latitude}, {longitude}], {{icon: waste_disposal_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Waste disposal');
 
-
+    recycling = ''
     recycling_query = """(node["amenity"="recycling"](around:{area},{lat},{lon}););out;""".format(area=area, lat=lat, lon=lon)
-    recycling = api.query(recycling_query)
-    for node in recycling.nodes:
-        markers += "markers.addLayer(L.marker([{latitude}, {longitude}], {{icon: recycling_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Recycling');
+    recycling_results = api.query(recycling_query)
+    for node in recycling_results.nodes:
+        recycling += "recycling.addLayer(L.marker([{latitude}, {longitude}], {{icon: recycling_icon}}).bindPopup('{name}'));".format(latitude=node.lat, longitude=node.lon, name='Recycling');
 
-
+    reported = ''
     active_requests = database.get_active_requests()
     for node in active_requests:
-        markers += "markers.addLayer(L.marker([{latitude}, {longitude}]).bindPopup('{name}'));".format(latitude=node[1], longitude=node[2], name='Reported trash<br>'+node[4]);
+        reported += "reported.addLayer(L.marker([{latitude}, {longitude}]).bindPopup('{name}'));".format(latitude=node[1], longitude=node[2], name='Reported trash<br>'+node[4]);
     
     
     # Render the page with the map
-    return render_template('map.html', markers=markers, lat=lat, lon=lon)
+    return render_template('map.html', baskets=baskets, disposal=disposal, recycling=recycling, reported=reported, lat=lat, lon=lon)
     
