ebc138fd93f50a6fdeb5818f91c846f7ce5fc31f
Mihaela Chirea <chireamihaela99@gmail.com>
2021-11-21 02:06:44 +0100

    Sign out

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def home():
 		if current_user == "":
 			return redirect('/login')
 		else:
-			return redirect('/'+current_user)
+			return redirect('/user/'+current_user)
 	return getMap()
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -66,18 +66,21 @@ def signup():
 			return result['msg']
 		return redirect('/login')
 
-@app.route('/<username>', methods = ['GET'])
+@app.route('/user/<username>', methods = ['GET', 'POST'])
 def profile(username):
+	if request.method == 'POST':
+		current_user = ""
+		return redirect('/home')
 	if request.method == 'GET':
 		# log_action("adela", "report", 1)
 		# log_action("adela", "pick", 20)
 		# add_user("adela", "adela", "Adela Vais", "+490668955844", "Arcisstraße 21, 80333 München")
 		user_data = database.get_user(username)
-		print(user_data)
+		# print(user_data)
 		# history1 = History(_type = "Reported trash at", points = 10, location = "Arcisstrasse")
 		# history2 = History(_type = "Picked up trash at", points = 100, location = "Marienplatz")
 		history_data = database.get_history(username) # [history1, history2]
-		print(history_data)
+		# print(history_data)
 		history = list(map(compute_history, history_data))
 		# history_points = map(lambda a: a.points, history)
 		return render_template(
@@ -93,7 +96,7 @@ def profile(username):
 			username = username,
 		) 
 
-@app.route('/edit/<username>', methods = ['POST', 'GET'])
+@app.route('/user/edit/<username>', methods = ['POST', 'GET'])
 def edit_user_data(username):
 	if request.method == 'GET':
 		user_data = database.get_user(username)
